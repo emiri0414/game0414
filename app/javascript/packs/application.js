86bcb547f03247fb67c3b2e545b1e6095462850e
vagrant <vagrant@localhost.localdomain>
2021-01-01 14:08:52 +0000

    js-change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery.raty.js')
+require('raty-js')
 
 var jQuery = require('jquery')
 global.$ = global.jQuery = jQuery;
